78c16d371e0117aaee7b37a9bd22b4acaaaebd94
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-17 14:14:16 -0800

    Add Lua bindings for Vector4i usertype

---
+++ src/luaopen_Vector4i.cpp
@@ -0,0 +1,25 @@
+#include "Runtime.h"
+
+void Runtime::luaopen_Vector4i() {
+	lua_state.new_usertype<godot::Vector4i>(
+		"Vector4i",
+		sol::constructors<Vector4i(), Vector4i(int, int, int, int)>(),
+		"x", &Vector4i::x,
+		"y", &Vector4i::y,
+		"z", &Vector4i::z,
+		"w", &Vector4i::w,
+		"abs", &Vector4i::abs,
+		"clamp", &Vector4i::clamp,
+		//"distanceSquaredTo", &Vector4i::distance_squared_to,
+		//"distanceTo", &Vector4i::distance_to,
+		"length", &Vector4i::length,
+		"lengthSquared", &Vector4i::length_squared,
+		"max", &Vector4i::max,
+		"maxAxisIndex", &Vector4i::max_axis_index,
+		"min", &Vector4i::min,
+		"minAxisIndex", &Vector4i::min_axis_index,
+		"sign", &Vector4i::sign,
+		"tostring", [](const Vector4i& v) { return std::string((v.operator String()).utf8().get_data()); }
+		//"snapped", &Vector4i::snapped
+	);
+}
