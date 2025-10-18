6781dd095daa52100d3491e431e4cf1bed50010f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-17 16:56:21 -0800

    Add luaopen_NativeObject_sandboxed function to expose NativeObject methods in a sandboxed environment

---
+++ src/luaopen_NativeObject_sandboxed.cpp
@@ -0,0 +1,64 @@
+#include "Runtime.h"
+#include "NativeObject.h"
+
+void Runtime::luaopen_NativeObject_sandboxed(const Array &classnames) {
+	lua_state.new_usertype<NativeObject>("NativeObject",
+            "new", sol::overload(
+                [classnames](std::string name) {
+                    if (!classnames.has( String(name.c_str()) ))
+                    {
+                        return static_cast<NativeObject*>(nullptr);
+                    }
+                    return new NativeObject(name);
+                },
+                [classnames](std::string name, Array args) {
+                    if (!classnames.has( String(name.c_str()) ))
+                    {
+                        return static_cast<NativeObject*>(nullptr);
+                    }
+                    return new NativeObject(name, args);
+                },
+                [classnames](std::string name, Array args, int scriptType) {
+                    if (!classnames.has( String(name.c_str()) ))
+                    {
+                        return static_cast<NativeObject*>(nullptr);
+                    }
+                    return new NativeObject(name, args, scriptType );
+                }
+            ),
+            "callStatic", [classnames](std::string classname, std::string methodname, Array args) {
+                if (!classnames.has( String(classname.c_str()) ))
+                {
+                    return Variant();
+                }
+                return NativeObject::callStatic( classname, methodname, args );
+            },
+            "getService", [classnames](std::string classname) {
+                if (!classnames.has( String(classname.c_str()) ))
+                {
+                    return static_cast<NativeObject*>(nullptr);
+                }
+                return NativeObject::getService( classname );
+            },
+            "call", &NativeObject::call,
+            "get", &NativeObject::get,
+            "set", &NativeObject::set,
+            "getClass", &NativeObject::getClass,
+            "isClass", &NativeObject::isClass,
+            "getMeta", sol::overload(
+                [](NativeObject* obj, std::string name) {
+                    return obj->getMeta( name );
+                },
+                [](NativeObject* obj, std::string name, Variant _default) {
+                    return obj->getMeta( name, _default );
+                }
+            ),
+            "getMetaList", &NativeObject::getMetaList,
+            "getMethodArgumentCount", &NativeObject::getMethodArgumentCount,
+            "getMethodList", &NativeObject::getMethodList,
+            "getPropertyList", &NativeObject::getPropertyList,
+            "hasMeta", &NativeObject::hasMeta,
+            "setMeta", &NativeObject::setMeta,
+            "isNull", &NativeObject::isNull
+        );
+}
