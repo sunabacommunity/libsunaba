bdf82709d25e612527567082a4d35eea1fa6dfd5
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-17 16:58:27 -0800

    Add luaopen_NativeReference function to expose NativeReference methods to Lua

---
+++ src/luaopen_NativeReference.cpp
@@ -0,0 +1,39 @@
+#include "Runtime.h"
+#include "NativeReference.h"
+
+void Runtime::luaopen_NativeReference() {
+	lua_state.new_usertype<NativeReference>("NativeReference",
+			"new", sol::overload(
+				[](std::string name) {
+					return std::make_unique<NativeReference>(name);
+				},
+				[](std::string name, Array args) {
+					return std::make_unique<NativeReference>(name, args);
+				},
+				[](std::string name, Array args, int scriptType) {
+					return std::make_unique<NativeReference>(name, args, scriptType);
+				}
+			),
+			"call", &NativeReference::call,
+			"get", &NativeReference::get,
+			"set", &NativeReference::set,
+			"getClass", &NativeReference::getClass,
+			"isClass", &NativeReference::isClass,
+			"getMeta", sol::overload(
+				[](NativeReference* obj, std::string name) {
+					return obj->getMeta( name );
+				},
+				[](NativeReference* obj, std::string name, Variant _default) {
+					return obj->getMeta( name, _default );
+				}
+			),
+			"getMetaList", &NativeReference::getMetaList,
+			"getMethodArgumentCount", &NativeReference::getMethodArgumentCount,
+			"getMethodList", &NativeReference::getMethodList,
+			"getPropertyList", &NativeReference::getPropertyList,
+			"hasMeta", &NativeReference::hasMeta,
+			"setMeta", &NativeReference::setMeta,
+			"isValid", &NativeReference::isValid,
+			"isNull", &NativeReference::isNull
+		);
+}
