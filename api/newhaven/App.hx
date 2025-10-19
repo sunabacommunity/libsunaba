a65abf9ac4a4e4b192bdce9b7011a5ccccd719bf
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-19 12:52:50 -0800

    Add App class with native Lua bindings for root and directory management

---
+++ api/newhaven/App.hx
@@ -0,0 +1,45 @@
+package newhaven;
+
+import newhaven.BaseClass;
+import haxe.macro.Expr.Catch;
+import newhaven.core.Variant;
+import newhaven.core.native.NativeObject;
+
+class App extends BaseClass {
+    public var rootNodeNative(get, default): NativeObject;
+	private function get_rootNodeNative(): NativeObject {
+		return untyped __lua__("_G.rootNode");
+	}
+
+    public static var execDir(get, default): String;
+    private static function get_execDir(): String {
+        return untyped __lua__("_G.execDir");
+    }
+
+    public static var resDir(get, default): String;
+    private static function get_resDir(): String {
+        return untyped __lua__("_G.resDir");
+    }
+
+    public static var shareDir(get, default): String;
+    private static function get_shareDir(): String {
+        return untyped __lua__("_G.shareDir");
+    }
+
+    public function new() {
+        //try {
+		init();
+        /*}
+        catch(e) {
+            ErrorD(e.toString());
+        }*/
+    }
+
+    public function init() {
+
+    }
+
+    public static function exit() {
+        untyped __lua__("_G.exit()");
+    }
+}
