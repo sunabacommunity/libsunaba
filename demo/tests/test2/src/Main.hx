5606395f63246a5fe6e096df38c7624082bc7993
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-19 15:44:07 -0800

    Refactor Main.hx to remove unnecessary trace statements for cleaner code

--- demo/tests/test2/src/Main.hx
+++ demo/tests/test2/src/Main.hx
@@ -16,36 +16,20 @@ class Main extends App {
 
 		var vec3 = new Vector3(1, 2, 3);
 		Sys.println("Vector3: " + vec3.toString());
-
-		trace("");
+		
 		var camera = new NativeObject("Camera3D");
-		trace("");
-		trace("Camera is null: " + camera.isNull());
-		trace("");
-		trace("Root Node Native is null: " + rootNodeNative.isNull());
-		trace(rootNodeNative.isClass("Node"));
 		rootNodeNative.call("add_child", args([camera]));
 
-		trace("");
 		var meshInstance = new NativeObject("MeshInstance3D");
-		trace("");
 		rootNodeNative.call("add_child", args([meshInstance]));
-		trace("");
 		meshInstance.set("position", new Vector3(0, 0, -5));
 
-		trace("");
 		var boxMesh = new NativeReference("BoxMesh");
 		boxMesh.set("size", new Vector3(1, 1, 1));
-		trace("");
 		meshInstance.set("mesh", boxMesh);
 
-
-		trace("");
 		var directionalLight = new NativeObject("DirectionalLight3D");
-		trace("");
 		rootNodeNative.call("add_child", args([directionalLight]));
-
-		trace("");
 	}
 
 	function args(args: Array<Variant>): ArrayList {
