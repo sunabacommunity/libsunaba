21a4a7184bc088496280c4c73ef90ddba00747b1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-18 19:21:44 -0800

    Load test application in runtime_test_1.gd

--- demo/runtime_test_1.gd
+++ demo/runtime_test_1.gd
@@ -1,12 +1,6 @@
 extends App
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	init(false, [])
-	do_string("print('Hello, World!')")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	load_app("res://tests/test1/bin/Test1.nhv")
