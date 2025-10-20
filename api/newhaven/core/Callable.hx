e2837ca6195f54c8090480bc236d00c561864c8c
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-19 20:47:16 -0800

    Add CallableNative class and abstract Callable for function handling

---
+++ api/newhaven/core/Callable.hx
@@ -0,0 +1,54 @@
+package newhaven.core;
+import newhaven.core.native.NativeObject;
+import newhaven.core.native.NativeReference;
+import haxe.Constraints.Function;
+
+@:native("Callable")
+extern class CallableNative {
+	public function new(func : Function);
+	@:native("new")
+	public static function createFromObject(object: BaseClass, method: String): CallableNative;
+	public function bind(arg: ArrayList): Callable;
+	public function call(args: ArrayList): Variant;
+	public function getArgumentCount(): Int;
+	public function getBoundArguments(): ArrayList;
+	public function getBoundArgumentsCount(): Int;
+	public function getObject(): NativeObject;
+	public function getReference(): Reference;
+	public function getUnboundArgumentsCount(): Int;
+	public function hash(): Int;
+	public function isCustom(): Bool;
+	public function isNull(): Bool;
+	public function isStandard(): Bool;
+	public function isValid(): Bool;
+	public function unbind(argcount: Int): CallableNative;
+}
+
+@:forward(
+	createFromObject,
+	bind,
+	call,
+	getArgumentCount,
+	getBoundArguments,
+	getBoundArgumentsCount,
+	getObject,
+	getReference,
+	getUnboundArgumentsCount,
+	hash,
+	isCustom,
+	isNull,
+	isStandard,
+	isValid,
+	unbind
+)
+abstract Callable(CallableNative) from CallableNative to CallableNative {
+	public function new(func: Function) {
+		this = new CallableNative(func);
+	}
+
+	@:from
+	public static function fromFunction(func: Function) {
+		return new CallableNative(func);
+	}
+}
+
