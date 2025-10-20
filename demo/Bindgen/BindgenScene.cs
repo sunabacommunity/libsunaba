82818f350865fc5cbbe8366affac165fee672bce
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-20 12:23:17 -0800

    refactor: implement file deletion logic for unwanted API files in BindgenScene

--- demo/Bindgen/BindgenScene.cs
+++ demo/Bindgen/BindgenScene.cs
@@ -8,8 +8,11 @@ public partial class BindgenScene : Node
 {
 	public override void _Ready()
 	{
-		//var enumBind = new EnumBind();
-		//enumBind.Build();
+		var enumBind = new EnumBind();
+		enumBind.Build();
+		foreach (var file in Directory.GetFiles("../enum_api/newhaven/"))
+			if (file.GetFile().Contains("_") && file.GetFile() != ".DS_Store")
+				File.Delete(file);
 		var enumBuilder = new EnumBuilder();
 		enumBuilder.Build();
 		foreach (var file in Directory.GetFiles("../api/newhaven/"))
