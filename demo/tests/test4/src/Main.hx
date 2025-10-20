7cddcbd30e0f5d4e30116bc49f71c58aae513e2a
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-20 15:25:12 -0800

    feat: implement callable invocation in init method

--- demo/tests/test4/src/Main.hx
+++ demo/tests/test4/src/Main.hx
@@ -1,6 +1,9 @@
-package demo.tests.test4.src;
+package;
 
 import newhaven.App;
+import haxe.Constraints.Function;
+import newhaven.core.Callable;
+import newhaven.core.ArrayList;
 
 class Main extends App {
     public static function main() {
@@ -8,7 +11,11 @@ class Main extends App {
     }
 
 	public override function init() {
-		
+		var func: Function = this.sayHello;
+		var callable: Callable = func;
+		var callArgs1 = new ArrayList();
+		callArgs1.append("World");
+		callable.call(callArgs1);
 	}
 
     public function sayHello(name: String) {
