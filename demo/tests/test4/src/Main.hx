22a649f59b6945ff0771e4762a61cd3a0ea50150
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-20 15:50:19 -0800

    fix: refactor init method to use args function for argument preparation

--- demo/tests/test4/src/Main.hx
+++ demo/tests/test4/src/Main.hx
@@ -4,6 +4,7 @@ import newhaven.App;
 import haxe.Constraints.Function;
 import newhaven.core.Callable;
 import newhaven.core.ArrayList;
+import newhaven.core.Variant;
 
 class Main extends App {
     public static function main() {
@@ -13,12 +14,18 @@ class Main extends App {
 	public override function init() {
 		var func: Function = this.sayHello;
 		var callable: Callable = func;
-		var callArgs1 = new ArrayList();
-		callArgs1.append("World");
-		callable.call(callArgs1);
+		callable.call(args(["World"]));
 	}
 
     public function sayHello(name: String) {
         Sys.println("Hello, " + name + "!");
     }
+
+    function args(args: Array<Variant>): ArrayList {
+		var newArgs = new ArrayList();
+		for (arg in args) {
+			newArgs.append(arg);
+		}
+		return newArgs;
+	}
 }
