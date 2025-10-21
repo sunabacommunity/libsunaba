26af1d7ffc624b8e1142bcea574387b9901bc030
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-21 09:23:59 -0800

    fix: add null check for object in call_func to prevent errors

--- demo/Engine/ScriptFunctionWrapper.gd
+++ demo/Engine/ScriptFunctionWrapper.gd
@@ -2,9 +2,12 @@ extends Object
 class_name ScriptFunctionWrapper
 
 var function: ScriptFunction = null
+var object: ScriptObject = null
 
 func call_func(...args) -> void:
 	var array = []
+	if object != null:
+		array.append(object)
 	for arg in args:
 		array.append(arg)
 	function.call_func(array)
