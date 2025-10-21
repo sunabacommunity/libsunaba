4a495cc7ba17bfe2ad2d6875f09dd3089ad22829
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-20 16:07:01 -0800

    fix: move initialization logic to _init method and restructure _ready method

--- demo/app_test_3.gd
+++ demo/app_test_3.gd
@@ -1,6 +1,17 @@
 extends App
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _init() -> void:
 	init(false, [])
 	load_app("res://tests/test3/bin/Test3.nhv")
+
+func _ready() -> void:
+	var window := get_window()
+	var displayScale := DisplayServer.screen_get_scale(window.current_screen)
+	if (OS.get_name() != "Linux"):
+		if OS.get_name() == "Windows":
+			var dpi = DisplayServer.screen_get_dpi(window.current_screen)
+			displayScale = dpi * 0.01
+		window.content_scale_factor = displayScale
+	window.size = Vector2i(1152, 648) * displayScale
+	window.min_size = Vector2i(1152, 648) * displayScale
