4f826bb6157aaeda915a07b3952d23e283b960ae
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-23 15:04:21 -0800

    Updated Test6 Main

--- demo/tests/test6/src/Main.hx
+++ demo/tests/test6/src/Main.hx
@@ -1,6 +1,11 @@
-package demo.tests.test6.src;
+package;
 
 import newhaven.App;
+import newhaven.Scene;
+import newhaven.GameObject;
+import test6.HelloComponent1;
+import test6.HelloComponent2;
+import test6.DeltaTimePrinter;
 
 class Main extends App {
     public static function main() {
@@ -8,6 +13,21 @@ class Main extends App {
     }
 
 	public override function init() {
-		Sys.println("Hello, World!");
+		var scene = new Scene();
+
+		var object1 = new GameObject();
+		scene.addObject(object1);
+		object1.addComponent(HelloComponent1);
+		object1.addComponent(HelloComponent2);
+
+		var child = new GameObject();
+		object1.addChild(child);
+		child.addComponent(HelloComponent1);
+
+		var object2 = new GameObject();
+		scene.addObject(object2);
+		object2.addComponent(DeltaTimePrinter);
+
+		rootNode.addChild(scene);
 	}
 }
