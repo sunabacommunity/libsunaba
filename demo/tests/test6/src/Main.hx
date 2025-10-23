eeb09d0d55e938cc62397e7e5e2d82881f969e15
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-23 14:29:12 -0800

    Added Test6 project

---
+++ demo/tests/test6/src/Main.hx
@@ -0,0 +1,13 @@
+package demo.tests.test6.src;
+
+import newhaven.App;
+
+class Main extends App {
+    public static function main() {
+        new Main();
+    }
+
+	public override function init() {
+		Sys.println("Hello, World!");
+	}
+}
