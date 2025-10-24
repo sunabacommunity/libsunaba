5c37ae6e49b4b36f9a26a658e450b721d2deeb12
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-24 14:27:15 -0800

    Add RegEx and RegExMatch classes for regular expression handling

---
+++ api/newhaven/RegEx.hx
@@ -0,0 +1,87 @@
+package newhaven;
+
+import newhaven.core.native.NativeReference;
+import newhaven.core.native.NativeObject;
+import newhaven.core.Variant;
+import newhaven.core.Reference;
+import newhaven.core.Object;
+import newhaven.core.Signal;
+import newhaven.core.ArrayList;
+import newhaven.core.TypedArray;
+
+class RegEx extends Reference {
+    public function new(?_native: NativeReference) {
+        super();
+        if (_native == null) {
+            _native = new NativeReference('RegEx');
+        }
+        native = _native;
+    }
+
+
+
+  public function clear(): Void {
+      var args = new ArrayList();
+      native.call('clear', args);
+  }
+  public function compile(pattern: String, ?showError: Bool): Int {
+      var args = new ArrayList();
+      args.append(pattern);
+      if (showError != null) {
+          args.append(showError);
+      }
+      return native.call('compile', args);
+  }
+  public static function createFromString(pattern: String, ?showError: Bool): RegEx {
+      var args = new ArrayList();
+      args.append(pattern);
+      if (showError != null) {
+          args.append(showError);
+      }
+      var ref: NativeReference = NativeObject.callStatic('RegEx', 'create_from_string', args);
+      return new RegEx(ref);
+  }
+  public function getGroupCount(): Int {
+      var args = new ArrayList();
+      return native.call('get_group_count', args);
+  }
+  public function getNames(): TypedArray<String> {
+      var args = new ArrayList();
+      return native.call('get_names', args);
+  }
+  public function getPattern(): String {
+      var args = new ArrayList();
+      return native.call('get_pattern', args);
+  }
+  public function isValid(): Bool {
+      var args = new ArrayList();
+      return native.call('is_valid', args);
+  }
+  public function search(subject: String, ?offset: Int, ?end: Int): RegExMatch {
+      var args = new ArrayList();
+      args.append(subject);
+      if (offset != null) {
+          args.append(offset);
+      }
+      if (end != null) {
+          args.append(end);
+      }
+      var ref: NativeReference = native.call('search', args);
+      return new RegExMatch(ref);
+  }
+  public function sub(subject: String, replacement: String, ?all: Bool, ?offset: Int, ?end: Int): String {
+      var args = new ArrayList();
+      args.append(subject);
+      args.append(replacement);
+      if (all != null) {
+          args.append(all);
+      }
+      if (offset != null) {
+          args.append(offset);
+      }
+      if (end != null) {
+          args.append(end);
+      }
+      return native.call('sub', args);
+  }
+}
