ea829a046807174ef086401ef7c0f992848b0bc5
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-24 15:23:12 -0800

    Added Test7 project

---
+++ demo/app_test_7.gd
@@ -0,0 +1,5 @@
+extends App
+
+func _init() -> void:
+	init(false, [])
+	load_app("res://tests/test7/bin/Test7.nhv")
---
+++ demo/tests/test7/src/Main.hx
@@ -0,0 +1,12 @@
+import newhaven.App;
+
+class Main extends App {
+    public static function main() {
+        new Main();
+    }
+
+	public override function init() {
+		var text = io.loadText("app://HelloWorld.txt");
+		trace(text);
+	}
+}
