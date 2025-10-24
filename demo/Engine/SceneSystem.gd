5e91da526c953349cca8251ed4a09d86567b0fd0
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-23 16:38:50 -0800

    Added Test6 test scene

---
+++ demo/Engine/SceneSystem.gd
@@ -0,0 +1,10 @@
+extends RefCounted
+
+func create_game_object():
+	return GameObject.new()
+
+func create_scene():
+	return Scene.new()
+
+func create_component():
+	return Component.new()
---
+++ demo/app_test_6.gd
@@ -0,0 +1,5 @@
+extends App
+
+func _init() -> void:
+	init(false, [])
+	load_app("res://tests/test6/bin/Test6.nhv")
