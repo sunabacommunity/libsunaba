4dce68a7404f1a46f6b52bccd1766c1ca4c78cb4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-27 14:35:24 -0800

    Added AABB haxe binding

---
+++ api/sunaba/core/AABB.hx
@@ -0,0 +1,32 @@
+package sunaba.core;
+
+extern class AABB {
+	public var end: Vector3;
+	public var position: Vector3;
+	public var size: Vector3;
+	public function abs(): AABB;
+	public function encloses(with: AABB): Bool;
+	public function expand(toPoint: Vector3): AABB;
+	public function getCenter(): Vector3;
+	public function getEndpoint(idx: Int): Vector3;
+	public function getLongestAxis(): Vector3;
+	public function getLongestAxisIndex(): Int;
+	public function getLongestAxisSize(): Float;
+	public function getShortestAxis(): Vector3;
+	public function getShortestAxisIndex(): Int;
+	public function getShortestAxisSize(): Float;
+	public function getSupport(direction: Vector3): Vector3;
+	public function getVolume(): Float;
+	public function grow(by: Float): AABB;
+	public function hasPoint(point: Vector3): Bool;
+	public function hasSurface(): Bool;
+	public function hasVolume(): Bool;
+	public function intersection(with: AABB): AABB;
+	public function intersects(with: AABB): Bool;
+	//public function intersectsPlane(plane: Plane)
+	public function intersectsRay(from: Vector3, dir: Vector3): Variant;
+	public function intersectsSegment(from: Vector3, to: Vector3): Variant;
+	public function isEqualApprox(aabb: AABB): Bool;
+	public function isFinite(): Bool;
+	public function merge(with: AABB): AABB;
+}
