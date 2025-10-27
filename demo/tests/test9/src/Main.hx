5cd9b14871875efad6887474015f8f88c745f6e1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-27 11:10:59 -0800

    Updated Test9 Main

--- demo/tests/test9/src/Main.hx
+++ demo/tests/test9/src/Main.hx
@@ -1,4 +1,13 @@
 import sunaba.App;
+import sunaba.SceneRoot;
+import sunaba.Entity;
+import sunaba.spatial.SpatialTransform;
+import sunaba.core.Vector3;
+import sunaba.spatial.Camera;
+import sunaba.spatial.mesh.MeshDisplay;
+import sunaba.spatial.mesh.BoxMesh;
+import myComponents.RotateComponent;
+import sunaba.Debug;
 
 class Main extends App {
     public static function main() {
@@ -6,6 +15,70 @@ class Main extends App {
     }
 
 	public override function init() {
-		Sys.println("Hello, World!");
+		var scene = new SceneRoot();
+		rootNode.addChild(scene);
+
+		var entity1 = new Entity();
+		entity1.name = "Entity1";
+		var e1Transform = entity1.addComponent(SpatialTransform);
+		scene.addEntity(entity1);
+		e1Transform.position = new Vector3(1, 2, 3);
+		var child1 = new Entity();
+		child1.name = "Child1";
+		var c1Transform = child1.addComponent(SpatialTransform);
+		entity1.addChild(child1);
+		c1Transform.position = new Vector3(4, 5, 6);
+		var entity2 = new Entity();
+		var e2Transform = entity2.addComponent(SpatialTransform);
+		scene.addEntity(entity2);
+		e2Transform.position = new Vector3(7, 8, 9);
+
+		var cameraEntity = new Entity();
+		cameraEntity.name = "Camera";
+		var cameraTransform = cameraEntity.addComponent(SpatialTransform);
+		var camera = cameraEntity.addComponent(Camera);
+		scene.addEntity(cameraEntity);
+		cameraTransform.position = new Vector3(0, 0, 1);
+		var boxEntity = new Entity();
+		boxEntity.name = "Box";
+		var boxTransform = boxEntity.addComponent(SpatialTransform);
+		var boxMeshDisplay = boxEntity.addComponent(MeshDisplay);
+		var boxMesh = boxEntity.addComponent(BoxMesh);
+		boxMesh.size = new Vector3(1, 1, 1);
+		var rotateComponent = boxEntity.addComponent(RotateComponent);
+		scene.addEntity(boxEntity);
+		boxTransform.position = new Vector3(0, 0, -1);
+
+		try {
+			printScene(scene);
+		}
+		catch (e) {
+			Debug.error("Error: " + e + " : " + e.stack);
+		}
+	}
+
+	public function printEntity(entity: Entity, indent: String = "    ") {
+		Sys.println(indent + "Entity: " + entity.name);
+		var transform = entity.getComponent(SpatialTransform);
+		if (transform != null) {
+			var position = transform.position;
+			var rotation = transform.rotation;
+			var scale = transform.scale;
+			var global = transform.globalTransform;
+			Sys.println(indent + "    Transform: " + position.toString() + ", " + rotation.toString() + ", " + scale.toString());
+			Sys.println(indent + "    GlobalTransform: " + global.toString());
+		}
+		for (child in 0...entity.getChildCount()) {
+			var childEntity = entity.getChild(child);
+			printEntity(childEntity, indent + "    ");
+		}
+	}
+
+	public function printScene(scene : SceneRoot) : Void {
+		Sys.println("Scene: " + scene);
+		for (entity in 0...scene.getEntityCount()) {
+			var entity = scene.getEntity(entity);
+			printEntity(entity);
+		}
 	}
 }
