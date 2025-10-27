73b1aa918d8672f17abffcddc556d65121514411
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-27 10:32:17 -0800

    Added MeshDisplay component

---
+++ api/sunaba/spatial/mesh/MeshDisplay.hx
@@ -0,0 +1,68 @@
+package sunaba.spatial.mesh;
+import sunaba.core.ArrayList;
+import sunaba.core.native.NativeObject;
+
+class MeshDisplay extends IGeometryInstance {
+	public function createConvexCollision(): Void {
+		node.native.call("create_convex_collision", new ArrayList());
+	}
+
+	public function createDebugTangents(): Void {
+		node.native.call("create_debug_tangents", new ArrayList());
+	}
+
+	public function findBlendShapeByName(name: String): Int {
+		var args = new ArrayList();
+		args.append(name);
+		return node.native.call("find_blend_shape_by_name", args);
+	}
+
+	public function getBlendShapeCount(): Int {
+		return node.native.call("get_blend_shape_count", new ArrayList());
+	}
+
+	public function getBlendShapeValue(index: Int): Float {
+		var args = new ArrayList();
+		args.append(index);
+		return node.native.call("get_blend_shape_value", args);
+	}
+
+	public function getSurfaceOverrideCount(): Int {
+		return node.native.call("get_surface_override_count", new ArrayList());
+	}
+
+	public function setBlendShapeValue(index: Int, value: Float): Void {
+		var args = new ArrayList();
+		args.append(index);
+		args.append(value);
+		node.native.call("set_blend_shape_value", args);
+	}
+
+	public function setMesh(res: Resource): Void {
+		if (!res.native.isClass("Mesh")) {
+			throw "Invalid mesh";
+		}
+		node.native.set("mesh", res.native);
+	}
+
+	public override function onInit() {
+		editorIconPath = "studio://icons/16/cushion-gray.png";
+		node = new Node(new NativeObject("MeshInstance3D"));
+
+		var transform: SpatialTransform = getComponent(SpatialTransform);
+		if (transform != null) {
+			if (transform.node != null) {
+				if (!transform.node.isNull()) {
+					if (!transform.node.native.isNull()) {
+						transform.node.addChild(node);
+					}
+				}
+			}
+		}
+	}
+
+	public override function onEnd() {
+		node.queueFree();
+		node = null;
+	}
+}
