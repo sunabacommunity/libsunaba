0045e71d19a1191196aca65bd01a56a6a71d962b
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-27 09:56:36 -0800

    Added IVisualInstance component

---
+++ api/sunaba/spatial/IVisualInstance.hx
@@ -0,0 +1,46 @@
+package sunaba.spatial;
+import sunaba.core.ArrayList;
+
+class IVisualInstance extends Behavior {
+	public var node: Node;
+
+	public var layerMask(get, set): Int;
+	function get_layerMask():Int {
+		return node.native.get("layer_mask");
+	}
+	function set_layerMask(value:Int):Int {
+		node.native.set("layer_mask", value);
+		return value;
+	}
+
+	public var sortingOffset(get, set): Float;
+	function get_sortingOffset():Float {
+		return node.native.get("sorting_offset");
+	}
+	function set_sortingOffset(value:Float):Float {
+		node.native.set("sorting_offset", value);
+		return value;
+	}
+
+	public var sortingUseAabbCenter(get, set): Bool;
+	function get_sortingUseAabbCenter():Bool {
+		return node.native.get("sorting_use_aabb_center");
+	}
+	function set_sortingUseAabbCenter(value:Bool):Bool {
+		node.native.set("sorting_use_aabb_center", value);
+		return value;
+	}
+
+	public function getLayerMaskValue(layer: Int): Bool {
+		var args = new ArrayList();
+		args.append(layer);
+		return node.native.call("get_layer_mask_value", args);
+	}
+
+	public function setLayerMaskValue(layer: Int, value: Bool): Void {
+		var args = new ArrayList();
+		args.append(layer);
+		args.append(value);
+		node.native.call("set_layer_mask_value", args);
+	}
+}
