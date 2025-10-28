f1a1d2501711f57791ade8eca0e273022f41a8e3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-28 13:39:15 -0800

    Updated Test11 Main

--- demo/tests/test11/src/Main.hx
+++ demo/tests/test11/src/Main.hx
@@ -1,4 +1,8 @@
 import sunaba.App;
+import sunaba.SceneRoot;
+import sunaba.Entity;
+import sunaba.spatial.SpatialTransform;
+import sunaba.SceneFile;
 
 class Main extends App {
     public static function main() {
@@ -6,6 +10,38 @@ class Main extends App {
     }
 
 	public override function init() {
-		Sys.println("Hello, World!");
+		var sceneFile = new SceneFile();
+		sceneFile.load("app://TestScene.vscn");
+
+		var scene = sceneFile.instance();
+
+		rootNode.addChild(scene);
+		printScene(scene);
+	}
+
+	public function printEntity(entity: Entity, indent: String = "    ") {
+		Sys.println(indent + "Entity: " + entity.name);
+		var transform = entity.getComponent(SpatialTransform);
+		if (transform != null) {
+			var position = transform.position;
+			var rotation = transform.rotation;
+			var scale = transform.scale;
+			var globalPosition = transform.globalPosition;
+			var globalRotation = transform.globalRotation;
+			Sys.println(indent + "    Transform: " + position.toString() + ", " + rotation.toString() + ", " + scale.toString());
+			Sys.println(indent + "    GlobalTransform: " + globalPosition.toString() + ", " + globalRotation.toString());
+		}
+		for (child in 0...entity.getChildCount()) {
+			var childEntity = entity.getChild(child);
+			printEntity(childEntity, indent + "    ");
+		}
+	}
+
+	public function printScene(scene : SceneRoot) : Void {
+		Sys.println("Scene: ");
+		for (entity in 0...scene.getEntityCount()) {
+			var entity = scene.getEntity(entity);
+			printEntity(entity);
+		}
 	}
 }
