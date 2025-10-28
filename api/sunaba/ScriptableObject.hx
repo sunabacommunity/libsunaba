437faa9171452bd2f73ab8076c70ad86b50056a1
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-28 11:51:18 -0800

    Added ScriptableObject class

---
+++ api/sunaba/ScriptableObject.hx
@@ -0,0 +1,41 @@
+package sunaba;
+
+import sunaba.io.IoInterface;
+import sunaba.core.Variant;
+import sunaba.core.Dictionary;
+
+abstract class ScriptableObject extends BaseClass {
+	public var path: String = "";
+
+	public var io: IoInterface;
+
+	public function getData() : Dictionary {
+		var data = new Dictionary();
+		data.set("path", path);
+		return data;
+	}
+
+	public function setData(data: Dictionary) : Void {
+		path = data.get("path");
+	}
+
+	public function load(?path: String): Void {
+		if (path == null) {
+			path = this.path;
+		}
+		var variant: Variant = io.loadVariant(path);
+		setData(variant);
+	}
+
+	public function save(?path: String) {
+		if (path == null) {
+			path = this.path;
+		}
+		var variant = getData();
+		io.saveVariant(path, variant);
+	}
+
+	public function new() {
+		io = untyped __lua__("_G.ioManager");
+	}
+}
