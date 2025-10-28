913a4a5a726d660ba105c86c2ca1bfd161906a8f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-28 08:31:49 -0800

    Updated Test10

--- demo/tests/test10/src/Main.hx
+++ demo/tests/test10/src/Main.hx
@@ -1,4 +1,8 @@
 import sunaba.App;
+import sunaba.ui.StyleBoxFlat;
+import sunaba.core.Color;
+import sunaba.DataUtils;
+import sunaba.JSON;
 
 class Main extends App {
     public static function main() {
@@ -6,6 +10,21 @@ class Main extends App {
     }
 
 	public override function init() {
-		Sys.println("Hello, World!");
+		var stylebox = new StyleBoxFlat();
+		stylebox.bgColor = Color.html("#ff2dff00");
+		stylebox.borderWidthBottom = 3;
+		stylebox.contentMarginLeft = 7.2;
+		var dictionary = DataUtils.varToDict(stylebox.native);
+
+		Sys.println(JSON.stringify(dictionary));
+
+		var newStyleBox = new StyleBoxFlat();
+		trace(newStyleBox.bgColor.toHtml());
+		trace(newStyleBox.borderWidthBottom);
+		trace(newStyleBox.contentMarginLeft);
+		newStyleBox = new StyleBoxFlat(DataUtils.dictToRes(dictionary));
+		trace(newStyleBox.bgColor.toHtml());
+		trace(newStyleBox.borderWidthBottom);
+		trace(newStyleBox.contentMarginLeft);
 	}
 }
