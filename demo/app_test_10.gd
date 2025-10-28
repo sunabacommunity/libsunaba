34de7f6d4a3b1dd4d7e6947268432320e23e1ad2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-28 10:53:20 -0800

    Added test scene for Test10

---
+++ demo/app_test_10.gd
@@ -0,0 +1,5 @@
+extends App
+
+func _ready() -> void:
+	init(false, [])
+	load_app("res://tests/test10/bin/Test10.snb")
--- demo/tests/test10/src/Main.hx
+++ demo/tests/test10/src/Main.hx
@@ -3,6 +3,10 @@ import sunaba.ui.StyleBoxFlat;
 import sunaba.core.Color;
 import sunaba.DataUtils;
 import sunaba.JSON;
+import sunaba.Debug;
+import haxe.Exception;
+import sunaba.core.ArrayList;
+import sunaba.core.Variant;
 
 class Main extends App {
     public static function main() {
@@ -10,21 +14,27 @@ class Main extends App {
     }
 
 	public override function init() {
-		var stylebox = new StyleBoxFlat();
-		stylebox.bgColor = Color.html("#ff2dff00");
-		stylebox.borderWidthBottom = 3;
-		stylebox.contentMarginLeft = 7.2;
-		var dictionary = DataUtils.varToDict(stylebox.native);
+		try {
+			var stylebox = new StyleBoxFlat();
+			stylebox.bgColor = Color.html("#ff2dff00");
+			stylebox.borderWidthBottom = 3;
+			stylebox.contentMarginLeft = 7.2;
+			var dictionary = DataUtils.varToDict(stylebox.native);
 
-		Sys.println(JSON.stringify(dictionary));
+			Sys.println(JSON.stringify(dictionary, "    "));
 
-		var newStyleBox = new StyleBoxFlat();
-		trace(newStyleBox.bgColor.toHtml());
-		trace(newStyleBox.borderWidthBottom);
-		trace(newStyleBox.contentMarginLeft);
-		newStyleBox = new StyleBoxFlat(DataUtils.dictToRes(dictionary));
-		trace(newStyleBox.bgColor.toHtml());
-		trace(newStyleBox.borderWidthBottom);
-		trace(newStyleBox.contentMarginLeft);
+			var newStyleBox = new StyleBoxFlat();
+			trace(newStyleBox.bgColor.toHtml());
+			trace(newStyleBox.borderWidthBottom);
+			trace(newStyleBox.contentMarginLeft);
+			newStyleBox = new StyleBoxFlat(DataUtils.dictToVar(dictionary, io));
+			trace(newStyleBox.bgColor.toHtml());
+			trace(newStyleBox.borderWidthBottom);
+			trace(newStyleBox.contentMarginLeft);
+		}
+		catch (e: Exception) {
+			trace(e);
+			Debug.error(e.message);
+		}
 	}
 }
