b22b3bd4e39cfcd6c3fb831514fa3ce8774988c6
Lucas C. Abbas <lucasbear9@gmail.com>
2025-10-29 09:55:07 -0800

    Updated SrciptLoader.gd

--- demo/Engine/SrciptLoader.gd
+++ demo/Engine/SrciptLoader.gd
@@ -1,6 +1,7 @@
-extends Node
+extends RefCounted
 
 func loadScript(classname: String, obj: Object):
+	if (obj == null): return
 	if (obj.get_script() == null):
 		var scriptPath = "res://Engine/AssetExtensions/" + classname + "AssetExtension.gd"
 		var script: Script = load(scriptPath)
