a8040e17db28513c07abec37f46ec385d1a67703
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-01 11:07:30 -0800

    Refactor Transform2D class to Transform2DNative and add abstract wrapper with operator overloads

--- api/sunaba/core/Transform2D.hx
+++ api/sunaba/core/Transform2D.hx
@@ -1,7 +1,7 @@
 package sunaba.core;
 
 @:native("Transform2D")
-extern class Transform2D {
+extern class Transform2DNative {
     public var origin : Vector2;
     public var x : Vector2;
     public var y : Vector2;
@@ -35,4 +35,123 @@ extern class Transform2D {
     public function translatedLocal(translation: Vector2): Transform2D;
     @:native("tostring")
     public function toString() : String;
+}
+
+@:forward(
+	origin,
+	x,
+	y,
+	zero,
+	newWithScaleAndSkew,
+	fromXYOrigin,
+	affineInverse,
+	basisXform,
+	basisXformInv,
+	determinant,
+	getOrigin,
+	getRotation,
+	getScale,
+	getSkew,
+	interpolateWith,
+	inverse,
+	isConformal,
+	isEqualApprox,
+	isFinite,
+	lookingAt,
+	orthonormalized,
+	rotated,
+	rotatedLocal,
+	scaled,
+	scaledLocal,
+	translated,
+	translatedLocal,
+	toString
+)
+abstract Transform2D(Transform2DNative) from Transform2DNative to Transform2DNative {
+	public inline function new(rotation : Float = 0, position : Vector2 = Vector2.ZERO) {
+		this = new Transform2DNative(rotation, position);
+	}
+
+	public static inline function zero() : Transform2D {
+		return Transform2DNative.zero();
+	}
+
+	public static inline function newWithScaleAndSkew(rotation: Float, scale: Vector2, skew: Float, position: Vector2) : Transform2D {
+		return Transform2DNative.newWithScaleAndSkew(rotation, scale, skew, position);
+	}
+
+	public static inline function fromXYOrigin(x: Vector2, y: Vector2, origin: Vector2) : Transform2D {
+		return Transform2DNative.fromXYOrigin(x, y, origin);
+	}
+
+	@:op([])
+	public inline function get(index : Int) : Vector2 {
+		switch(index) {
+			case 0: return this.x;
+			case 1: return this.y;
+			case 2: return this.origin;
+			default: throw "Index out of bounds: " + index;
+		}
+	}
+
+	@:op([])
+	public inline function set(index : Int, value : Vector2) : Vector2 {
+		switch(index) {
+			case 0: this.x = value;
+			case 1: this.y = value;
+			case 2: this.origin = value;
+			default: throw "Index out of bounds: " + index;
+		}
+		return value;
+	}
+
+	@:op(A * B)
+	public inline function multiply(other : Transform2D) : Transform2D {
+		return new Transform2DNative(
+			this.getRotation() + other.getRotation(),
+			this.basisXform(other.getOrigin()) + this.getOrigin()
+		);
+	}
+
+	@:op(A == B)
+	public inline function equals(other : Transform2D) : Bool {
+		return this.x == other.x && this.y == other.y && this.origin == other.origin;
+	}
+
+	@:op(A != B)
+	public inline function notEquals(other : Transform2D) : Bool {
+		var og: Transform2D = this;
+		return !og.equals(other);
+	}
+
+	@:op(A * B)
+	public inline function multiplyVector2(other : Vector2) : Vector2 {
+		return this.basisXform(other) + this.getOrigin();
+	}
+
+	@:op(A * B)
+	public inline function multiplyScalar(other : Float) : Transform2D {
+		return new Transform2DNative(
+			this.getRotation() * other,
+			this.getOrigin() * other
+		);
+	}
+
+	@:op(A / B)
+	public inline function divideScalar(other : Float) : Transform2D {
+		return new Transform2DNative(
+			this.getRotation() / other,
+			this.getOrigin() / other
+		);
+	}
+
+	@:op(A * B)
+	public inline function multiplyIntScalar(other : Int) : Transform2D {
+		return this.multiplyScalar(cast other);
+	}
+
+	@:op(A / B)
+	public inline function divideIntScalar(other : Int) : Transform2D {
+		return this.divideScalar(cast other);
+	}
 }
