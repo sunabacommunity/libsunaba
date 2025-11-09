ff03944c1f538aa4ba40115039bf5317fd614c90
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-09 10:41:50 -0900

    fix: override nativeInit method in DesktopAppView for proper initialization

--- api/sunaba/DesktopAppView.hx
+++ api/sunaba/DesktopAppView.hx
@@ -4,8 +4,7 @@ import sunaba.core.native.ScriptType;
 import sunaba.core.ArrayList;
 
 class DesktopAppView extends AppView {
-	public function new(?_native: NativeObject) {
-		super();
+	public override function nativeInit(?_native: NativeObject) {
 		if (_native == null) {
 			_native = new NativeObject("res://Engine/DesktopApp.gd", new ArrayList(), ScriptType.gdscript);
 		}
