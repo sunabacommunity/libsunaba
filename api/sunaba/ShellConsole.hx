ad1da0d5277f9575dc712838c592d65379e71a60
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-13 14:36:13 -0900

    feat: implement ShellConsole class for command execution and Lua integration

---
+++ api/sunaba/ShellConsole.hx
@@ -0,0 +1,61 @@
+package sunaba;
+import sunaba.core.Reference;
+import sunaba.core.native.NativeReference;
+import sunaba.core.ArrayList;
+import sunaba.core.native.ScriptType;
+import sunaba.core.TypedArray;
+import sunaba.core.Variant;
+import sunaba.core.VariantType;
+
+class ShellConsole extends Reference {
+	public override function nativeInit(?_native: NativeReference) {
+		if (_native == null) {
+			_native = new NativeReference("res://Engine/ShellConsole.cs", new ArrayList(), ScriptType.csharp);
+		}
+		native = _native;
+
+		var commandArray = (args: TypedArray<String>) -> {
+			for (i in 0...args.size()) {
+				var arg = args.get(i);
+				cmd(arg);
+			}
+		}
+		addCommand("cmd", commandArray);
+		eval("$ = function(command) cmd(command) end");
+	}
+
+	public function addCommand(name: String, func: TypedArray<String>->Void) {
+		var args = new ArrayList();
+		args.append(name);
+		args.append(func);
+		native.call("AddCommand", args);
+	}
+
+	public function eval(code: String) {
+		var args = new ArrayList();
+		args.append(code);
+		var variant: Variant = native.call("DoCode", args);
+		if (variant.getType() == VariantType.string) {
+			throw variant.toString();
+		}
+	}
+
+	public function cmd(command: String) {
+		var parsedCommand = CommandParser.parseCommand(command);
+		if (parsedCommand == null) {
+			throw "Invalid command format";
+			return;
+		}
+
+		var luastr = "__command('" + parsedCommand.name + "', {";
+		for (i in 0...parsedCommand.args.length) {
+			var arg = parsedCommand.args[i];
+			luastr += "'" + arg + "'";
+			if (i != parsedCommand.args.length - 1) {
+				luastr += ",";
+			}
+		}
+		luastr += ("})");
+		eval(luastr);
+	}
+}
