9a38b9e53068778cd906bde36c8a037a9b78dfdb
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-19 17:20:25 -0900

    feat: update LibraryLoadResult to include error as String and modify loadLibrary methods to return Void

--- api/sunaba/LibraryLoader.hx
+++ api/sunaba/LibraryLoader.hx
@@ -9,10 +9,13 @@ import haxe.Exception;
 import sys.FileSystem;
 import sys.io.File;
 
-@:multiReturn
-extern class LibraryLoadResult {
-	var chunk: Dynamic;
-	var err: Exception;
+class LibraryLoadResult {
+	public var chunk: Dynamic;
+	public var err: String;
+	public function new(chunk: Dynamic, err: String) {
+		this.chunk = chunk;
+		this.err = err;
+	}
 }
 
 class LibraryLoader extends BaseClass {
@@ -28,25 +31,59 @@ class LibraryLoader extends BaseClass {
 
 	public var libraryName: String = "library";
 
+	public var res: LibraryLoadResult = null;
+
 	public function new() {
 		_env = Table.create();
 		var globalEnv: Table<Dynamic, Dynamic> = untyped __lua__("_G");
-		Lua.setmetatable(_env, globalEnv);
+		untyped __lua__("
+			for key, value in pairs(_G) do
+				self._env[key] = value
+			end
+		");
 		var ioNative: NativeReference = untyped __lua__("_G.__ioManager");
 		io = new IoManager(ioNative);
 	}
 
-	public function loadLibrary(path: String): LibraryLoadResult {
+	public function loadLibrary(path: String): Void {
 		var env = this._env;
-		var code = io.loadText(path);
-		var libName = this.libraryName;
-		return untyped __lua__("load(code, libname, 't', env)");
+		var code: String = io.loadText(path);
+		var libName: String = this.libraryName;
+
+		untyped __lua__("
+        local __chunk, __err = load(code, libName, 't', env)
+        rawset(_G, '__sunaba_chunk', __chunk)
+        rawset(_G, '__sunaba_err', __err)
+    ");
+
+		var chunk: Dynamic = untyped __lua__("_G.__sunaba_chunk");
+		var err   : Dynamic = untyped __lua__("_G.__sunaba_err");
+
+		untyped __lua__("_G.__sunaba_chunk = nil");
+		untyped __lua__("_G.__sunaba_err = nil");
+
+		res = new LibraryLoadResult(chunk, err);
 	}
 
-	public function loadLibraryFileSystem(path: String): LibraryLoadResult {
+
+	public function loadLibraryFileSystem(path: String): Void {
 		var env = this._env;
-		var code = File.getContent(path);
+		var code: String = File.getContent(path);
 		var libName = this.libraryName;
-		return untyped __lua__("load(code, libname, 't', env)");
+
+		untyped __lua__("
+        local __chunk, __err = load(code, libName, 't', env)
+        rawset(_G, '__sunaba_chunk', __chunk)
+        rawset(_G, '__sunaba_err', __err)
+    ");
+
+		var chunk: Dynamic = untyped __lua__("_G.__sunaba_chunk");
+		var err   : Dynamic = untyped __lua__("_G.__sunaba_err");
+
+		untyped __lua__("_G.__sunaba_chunk = nil");
+		untyped __lua__("_G.__sunaba_err = nil");
+
+		res = new LibraryLoadResult(chunk, err);
 	}
+
 }
