ab938111e8708666364a02893964ee097d118ad4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-20 10:53:32 -0900

    fix: refine property iteration in dictToRes to exclude 'script' and 'asset_path'

--- api/sunaba/DataUtils.hx
+++ api/sunaba/DataUtils.hx
@@ -612,7 +612,10 @@ class DataUtils {
 			for (i in 0...propertyList.size()) {
 				var prop: Dictionary = propertyList.get(i);
 				var name: String = prop.get("name");
-				if (name == "script" || name == "asset_path") {
+				if (name == "script") {
+					continue;
+				}
+				if (name == "asset_path") {
 					continue;
 				}
 				if (res.get(name).getType() == VariantType.nil) {
