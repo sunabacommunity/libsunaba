66e6502a7e3d371cfa746a65af32434a96f9a3c4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-01 18:39:24 -0900

    Refactor Signal class to SignalNative and introduce abstract Signal for improved functionality

--- api/sunaba/core/Signal.hx
+++ api/sunaba/core/Signal.hx
@@ -2,9 +2,10 @@ package sunaba.core;
 
 import sunaba.core.native.NativeObject;
 import sunaba.core.native.NativeReference;
+import haxe.Constraints.Function;
 
 @:native("Signal")
-extern class Signal {
+extern class SignalNative {
 	public function new();
 	@:native("new")
 	public static function createFromObject(object: NativeObject, signal: String): Signal;
@@ -19,4 +20,39 @@ extern class Signal {
 	public function hasConnections(): Bool;
 	public function isConnected(callable: Callable): Bool;
 	public function isNull(): Bool;
+}
+
+@:forward(
+	connect,
+	disconnect,
+	getConnections,
+	getName,
+	getObject,
+	getReference,
+	hasConnections,
+	isConnected,
+	isNull
+)
+abstract Signal(SignalNative) from SignalNative to SignalNative {
+	public function new() {
+		this = new SignalNative();
+	}
+
+	public static function createFromObject(object: NativeObject, signal: String): Signal {
+		return SignalNative.createFromObject(object, signal);
+	}
+
+	public static function createFromReference(ref: NativeReference, signal: String): Signal {
+		return SignalNative.createFromReference(ref, signal);
+	}
+
+	public function add(func: Function): Callable {
+		var callable = Callable.fromFunction(func);
+		this.connect(callable);
+		return callable;
+	}
+
+	public function remove(callable: Callable) {
+		this.disconnect(callable);
+	}
 }
