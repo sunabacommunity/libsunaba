d6e088cbadd3d31650dbb8cfe0649c2908c885bf
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-10 14:04:33 -0900

    Add native methods for getInt64 and setInt64 in ByteObject for enhanced data manipulation

--- api/sunaba/core/ByteObject.hx
+++ api/sunaba/core/ByteObject.hx
@@ -10,10 +10,12 @@ extern class ByteObject  {
     @:native("new")
     public static function fromFloat(value:Float):ByteObject;
 
+    @:native("getInt64")
     public function getInt() :Int;
 
     public function getFloat() :Float;
 
+    @:native("setInt64")
     public function setInt(value:Int):Void;
 
     public function setFloat(value:Float):Void;
