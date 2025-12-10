8a77522d9e7d7a9fee69cb54d54730d6a2b9c9bb
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-10 14:04:41 -0900

    Add native methods for getInt64 and setInt64 in Byte for improved data handling

--- src/io/Byte.cpp
+++ src/io/Byte.cpp
@@ -9,8 +9,17 @@ void io ::luaopen_Byte(sol::state& lua) {
 			[](int value) { return new Byte(value); }
 		),
 		"getInt", &Byte::getInt,
+		"getInt64", [](Byte* byte) {
+			int i = byte->getInt();
+			int64_t i64 = i;
+			return i64;
+		},
 		"getFloat", &Byte::getDouble,
 		"setInt", &Byte::setInt,
+		"setInt64", [](Byte* byte, int64_t i64) {
+			int i = i64;
+			byte->setInt(i64);
+		},
 		"setFloat", &Byte::setDouble
 	);
 }
