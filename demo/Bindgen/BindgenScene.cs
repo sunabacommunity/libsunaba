d06d1b9ea10ca4145decf321a6c59d104ab1c064
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-14 12:28:02 -0900

    Comment out unused EnumBind initialization in BindgenScene

--- demo/Bindgen/BindgenScene.cs
+++ demo/Bindgen/BindgenScene.cs
@@ -8,11 +8,11 @@ public partial class BindgenScene : Node
 {
 	public override void _Ready()
 	{
-		var enumBind = new EnumBind();
+		/*var enumBind = new EnumBind();
 		enumBind.Build();
 		foreach (var file in Directory.GetFiles("../enum_api/sunaba/"))
 			if (file.GetFile().Contains("_") && file.GetFile() != ".DS_Store")
-				File.Delete(file);
+				File.Delete(file);*/
 		var enumBuilder = new EnumBuilder();
 		enumBuilder.Build();
 		foreach (var file in Directory.GetFiles("../api/sunaba/"))
