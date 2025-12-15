51df7533403e83fc0af5bd1cf07e7aa248f4e11f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-15 09:44:14 -0900

    Added SphereMesh component class

---
+++ api/sunaba/spatial/mesh/SphereMesh.hx
@@ -0,0 +1,78 @@
+package sunaba.spatial.mesh;
+
+import sunaba.core.Dictionary;
+import sunaba.core.native.NativeReference;
+
+class SphereMesh extends PrimitiveMesh {
+    public var height(get, set): Float;
+	function get_height():Float {
+		return res.native.get("height");
+	}
+	function set_height(value:Float):Float {
+		res.native.set("height", value);
+		return value;
+	}
+
+    public var isHemisphere(get, set): Bool;
+    function get_isHemisphere():Bool {
+        return res.native.get("is_hemisphere");
+    }
+    function set_isHemisphere(value:Bool):Bool {
+        res.native.set("is_hemisphere", value);
+        return value;
+    }
+
+    public var radialSegments(get, set): Int;
+    function get_radialSegments():Int {
+        return res.native.get("radial_segments");
+    }
+    function set_radialSegments(value:Int):Int {
+        res.native.set("radial_segments", value);
+        return value;
+    }
+
+    public var radius(get, set): Float;
+    function get_radius():Float {
+        return res.native.get("radius");
+    }
+    function set_radius(value:Float):Float {
+        res.native.set("radius", value);
+        return value;
+    }
+
+    public var rings(get, set): Int;
+    function get_rings():Int {
+        return res.native.get("rings");
+    }
+    function set_rings(value:Int):Int {
+        res.native.set("rings", value);
+        return value;
+    }
+
+    public override function getData():Dictionary {
+        var data = super.getData();
+
+        data.set("height", height);
+        data.set("isHemisphere", isHemisphere);
+        data.set("radialSegments", radialSegments);
+        data.set("radius", radius);
+        data.set("rings", rings);
+
+        return data;
+    }
+
+    public override function setData(data:Dictionary) {
+        super.setData(data);
+
+        height = data.get("height");
+        isHemisphere = data.get("isHemisphere");
+        radialSegments = data.get("radialSegments");
+        radius = data.get("radius");
+        rings = data.get("rings");
+    }
+
+    public override function onInit() {
+        res = new Resource(new NativeReference("SphereMesh"));
+        super.onInit();
+    }
+}
