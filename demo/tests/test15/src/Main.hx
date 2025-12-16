6900e95bca7493fa98b0b5994c8a0a5faab0eea2
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-15 17:05:30 -0900

    Updated test scene in Test15 to include rigidbody tests

--- demo/tests/test15/src/Main.hx
+++ demo/tests/test15/src/Main.hx
@@ -9,10 +9,16 @@ import sunaba.core.native.NativeObject;
 import myComponents.FreeLook3D;
 import sunaba.spatial.Camera;
 import sunaba.spatial.mesh.BoxMesh;
+import sunaba.spatial.mesh.SphereMesh;
+import sunaba.spatial.mesh.CapsuleMesh;
+import sunaba.spatial.mesh.CylinderMesh;
 import sunaba.spatial.mesh.MeshDisplay;
 import sunaba.spatial.lighting.DirectionalLight;
 import sunaba.spatial.physics.StaticBody;
 import sunaba.spatial.physics.BoxShape;
+import sunaba.spatial.physics.SphereShape;
+import sunaba.spatial.physics.CapsuleShape;
+import sunaba.spatial.physics.CylinderShape;
 
 class Main extends App {
 	public static function main() {
