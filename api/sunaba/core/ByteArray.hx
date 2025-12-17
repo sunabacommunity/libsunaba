e452f9c369ab72dfc44a75d50b9d3005cc51b7b3
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-16 16:34:12 -0900

    refactor ByteArray functions to use ByteArrayUtils for improved clarity and maintainability

--- api/sunaba/core/ByteArray.hx
+++ api/sunaba/core/ByteArray.hx
@@ -25,19 +25,31 @@ extern class ByteArray {
     public function toTable() : TypedArray<Float>;
 }
 
+@:deprecated("Use 'ByteArrayUtils.binaryDataToBytes' instead")
 inline function BinaryDataToBytes(binaryData: ByteArray) : Bytes {
-    var size = binaryData.size();
-    var bytes = Bytes.alloc(size);
-    for (i in 0...size) {
-        bytes.set(i, binaryData.get(i).getInt());
-    }
-    return bytes;
+    return ByteArrayUtils.binaryDataToBytes(binaryData);
 }
 
+@:deprecated("Use 'ByteArrayUtils.bytesToBinaryData' instead")
 inline function BytesToBinaryData(bytes: Bytes) : ByteArray {
-    var binaryData = new ByteArray();
-    for (i in 0...bytes.length) {
-        binaryData.append(ByteObject.fromInt(bytes.get(i)));
-    }
-    return binaryData;
+    return ByteArrayUtils.bytesToBinaryData(bytes);
+}
+
+class ByteArrayUtils {
+	public static inline function binaryDataToBytes(binaryData: ByteArray) : Bytes {
+		var size = binaryData.size();
+		var bytes = Bytes.alloc(size);
+		for (i in 0...size) {
+			bytes.set(i, binaryData.get(i).getInt());
+		}
+		return bytes;
+	}
+
+	public static inline function bytesToBinaryData(bytes: Bytes) : ByteArray {
+		var binaryData = new ByteArray();
+		for (i in 0...bytes.length) {
+			binaryData.append(ByteObject.fromInt(bytes.get(i)));
+		}
+		return binaryData;
+	}
 }
