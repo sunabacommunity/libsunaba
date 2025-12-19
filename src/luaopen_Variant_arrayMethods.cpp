5dfdfac027487bd34588d2a253735e9a7be83265
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-19 12:55:05 -0900

    Fix asIntArray64 method to handle PACKED_INT32_ARRAY conversion

--- src/luaopen_Variant_arrayMethods.cpp
+++ src/luaopen_Variant_arrayMethods.cpp
@@ -24,7 +24,14 @@ void Runtime::luaopen_Variant_arrayMethods() {
 
     variant_type["asIntArray64"] = [](const Variant& v) {
         std::vector<int64_t> data;
-        if (v.get_type() != Variant::PACKED_INT64_ARRAY) return data;
+        if (v.get_type() != Variant::PACKED_INT64_ARRAY && v.get_type() != Variant::PACKED_INT32_ARRAY) return data;
+    	if (v.get_type() == Variant::PACKED_INT32_ARRAY) {
+    		PackedInt32Array packed_data = v;
+    		for (int i = 0; i < packed_data.size(); ++i) {
+    			data.push_back(packed_data[i]);
+    		}
+    		return data;
+    	}
         PackedInt64Array packed_data = v;
         for (int i = 0; i < packed_data.size(); ++i) {
             data.push_back(packed_data[i]);
