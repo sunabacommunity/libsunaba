7d55ea83c66ef00d7fbe3e7b026c2a229f43dbb4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-22 11:31:07 -0900

    Add surfacePrimitiveTypes handling in MeshData class

--- api/sunaba/spatial/mesh/MeshData.hx
+++ api/sunaba/spatial/mesh/MeshData.hx
@@ -6,6 +6,7 @@ import sunaba.core.StringArray;
 class MeshData extends ScriptableObject {
 	public var surfaceNames: Array<String> = new Array();
 	public var surfaces: ArrayList = new ArrayList();
+	public var surfacePrimitiveTypes: ArrayList = new ArrayList();
 	public var surfaceMaterials: ArrayList = new ArrayList();
 
 	public static function fromImporterMesh(importerMesh: ImporterMesh) {
@@ -13,6 +14,9 @@ class MeshData extends ScriptableObject {
 		for (i in 0...importerMesh.getSurfaceCount()) {
 			var surfaceArrays = importerMesh.getSurfaceArrays(i);
 			meshData.surfaces.append(surfaceArrays);
+
+			var surfacePrimitiveType: Int = importerMesh.getSurfacePrimitiveType(i);
+			meshData.surfacePrimitiveTypes.append(surfacePrimitiveType);
 
 			var surfaceMaterial = importerMesh.getSurfaceMaterial(i);
 			meshData.surfaceMaterials.append(surfaceMaterial);
@@ -29,6 +33,7 @@ class MeshData extends ScriptableObject {
 
 		data.set("surfaceNames", DataUtils.varToDict(StringArray.fromArray(surfaceNames)));
 		data.set("surfaces", DataUtils.varToDict(surfaces));
+		data.set("surfacePrimitiveTypes", DataUtils.varToDict(surfacePrimitiveTypes));
 		data.set("surfaceMaterials", DataUtils.varToDict(surfaceMaterials));
 
 		return data;
@@ -39,6 +44,7 @@ class MeshData extends ScriptableObject {
 
 		surfaceNames = DataUtils.dictToVar(data.get("surfaceNames")).toStringArray();
 		surfaces = DataUtils.dictToVar(data.get("surfaces"));
+		surfacePrimitiveTypes = DataUtils.dictToVar(data.get("surfacePrimitiveTypes"));
 		surfaceMaterials = DataUtils.dictToVar(data.get("surfaceMaterials"));
 	}
 }
