8fc68668ef9b537037ee1f8d77d0b2afccaa893f
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-23 19:26:26 -0900

    Comment out print_type logging for cleaner output during execution

--- src/example_class.cpp
+++ src/example_class.cpp
@@ -5,5 +5,5 @@ void ExampleClass::_bind_methods() {
 }
 
 void ExampleClass::print_type(const Variant &p_variant) const {
-	print_line(vformat("Type: %d", p_variant.get_type()));
+	//print_line(vformat("Type: %d", p_variant.get_type()));
 }
