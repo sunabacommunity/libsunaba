7a208cf7542099566d20119d695b8c56d1797e67
Lucas C. Abbas <lucasbear9@gmail.com>
2026-01-26 17:34:46 -0900

    Updated Map3D

--- api/sunaba/spatial/Map3D.hx
+++ api/sunaba/spatial/Map3D.hx
@@ -13,13 +13,39 @@ class Map3D extends Behavior {
 		return null;
 	}
 
+	public function getAllTriggers(): Array<Trigger> {
+		var triggers = new Array();
+		for (i in 0...node.getChildCount()) {
+			var childNode = node.getChild(i);
+			if (childNode.native.isClass("Area3D")) {
+				triggers.push(new Trigger(scene, childNode.native));
+			}
+		}
+		return triggers;
+	}
+
 	public function getWater(name: String): Null<Water> {
 		var childNode = node.getNode(name);
 		if (!childNode.isNull()) {
 			if (childNode.native.isClass("Area3D")) {
-				return new Water(scene, childNode.native);
+				if(childNode.native.get("isWater").getType() == 1) {
+					return new Water(scene, childNode.native);
+				}
 			}
 		}
 		return null;
+	}
+
+	public function getAllWaterTriggers(): Array<Water> {
+		var waterTriggers = new Array();
+		for (i in 0...node.getChildCount()) {
+			var childNode = node.getChild(i);
+			if (childNode.native.isClass("Area3D")) {
+				if(childNode.native.get("isWater").getType() == 1) {
+					waterTriggers.push(new Water(scene, childNode.native));
+				}
+			}
+		}
+		return waterTriggers;
 	}
 }
