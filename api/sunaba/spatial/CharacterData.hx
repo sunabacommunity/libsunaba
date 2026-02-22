6e26c96b60bebc5d81bbbf869eb281e0cbfa41ca
Lucas C. Abbas <lucasbear9@gmail.com>
2026-02-22 08:20:00 -0900

    Updated CharacterData.hx

--- api/sunaba/spatial/CharacterData.hx
+++ api/sunaba/spatial/CharacterData.hx
@@ -5,30 +5,36 @@ import sunaba.core.Dictionary;
 import sunaba.core.ArrayList;
 
 class CharacterData extends ScriptableObject {
+	public var name: String;
+	public var desc: String;
 	public var skinTone: Color = new Color(1, 1, 1, 1);
 	public var bodyType: BodyType = BodyType.male;
 	public var femaleChestSize: Float = 0.0;
 	public var legThickness: Float = 0.0;
 	public var maleArmThickness: Float = 0.0;
 
-	public var faceTexture: Texture2D;
+	public var faceTexture: FaceTextureData;
 
-	public var clothes: Array<Texture2D> = new Array();
+	public var clothes: Array<Clothing> = new Array();
 
-	public var headwearList: Array<Prefab> = new Array();
+	public var femaleDress: FemaleDress;
+
+	public var headwearList: Array<Headwear> = new Array();
 
 	public override function getData() {
 		var data = super.getData();
 
+		data.set("name", name);
+		data.set("desc", desc);
 		data.set("skinTone", DataUtils.varToDict(skinTone));
 		data.set("bodyType", Variant.fromInt(bodyType));
 		data.set("femaleChestSize", femaleChestSize);
 		data.set("legThickness", legThickness);
 		data.set("maleArmThickness", maleArmThickness);
-		data.set("faceTextures", DataUtils.varToDict(faceTexture.native));
+		data.set("faceTextures",faceTexture.getData());
 		var clothesArr = new ArrayList();
 		for (clothing in clothes) {
-			clothesArr.append(DataUtils.varToDict(clothing));
+			clothesArr.append(clothing.getData());
 		}
 		data.set("clothes", clothesArr);
 		var headwearArr = new ArrayList();
@@ -43,23 +49,24 @@ class CharacterData extends ScriptableObject {
 	public override function setData(data: Dictionary) {
 		super.setData(data);
 
+		name = data.get("name");
+		desc = data.get("desc");
 		skinTone = DataUtils.dictToVar(data.get("skinTone"));
 		bodyType = data.get("bodyType");
 		femaleChestSize = data.get("femaleChestSize");
 		legThickness = data.get("legThickness");
 		maleArmThickness = data.get("maleArmThickness");
-		faceTexture = new Texture2D(DataUtils.dictToVar(data.get("faceTexture")));
+		faceTexture = new FaceTextureData();
+		faceTexture.setData(data.get("faceTexture"));
 		var clothesArr: ArrayList = data.get("clothes");
-		for (clothing in clothesArr) {
-			clothes.push(
-				new Texture2D(
-					DataUtils.dictToVar(clothing)
-				)
-			);
+		for (clothingData in clothesArr) {
+			var clothing = new Clothing();
+			clothing.setData(clothingData);
+			clothes.push(clothing);
 		}
 		var headwearArr: ArrayList = data.get("headwearList");
 		for (headwearData in headwearArr) {
-			var headwear = new Prefab();
+			var headwear = new Headwear();
 			headwear.setData(headwearData);
 			headwearList.push(headwear);
 		}
