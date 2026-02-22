42d266471d52bf71b3e9b73f9e7bb1e076f3c17f
Lucas C. Abbas <lucasbear9@gmail.com>
2026-02-22 08:19:07 -0900

    Added FaceTextureData class

---
+++ api/sunaba/spatial/FaceTextureData.hx
@@ -0,0 +1,26 @@
+﻿package sunaba.spatial;
+import sunaba.core.Dictionary;
+
+class FaceTextureData extends ScriptableObject {
+	public var name: String;
+	public var desc: String;
+	public var texture: Texture2D;
+
+	public override function getData() {
+		var data = super.getData();
+
+		data.set("name", name);
+		data.set("desc", desc);
+		data.set("texture", DataUtils.varToDict(texture.native));
+
+		return data;
+	}
+
+	public override function setData(data: Dictionary) {
+		super.setData(data);
+
+		name = data.get("name");
+		desc = data.get("desc");
+		texture = new Texture2D(DataUtils.dictToVar(data.get("texture")));
+	}
+}
