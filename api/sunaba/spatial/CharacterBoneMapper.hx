bf34e26d92aa6cb651b58bef424a99409863187f
Lucas C. Abbas <lucasbear9@gmail.com>
2026-02-23 13:54:30 -0900

    Added a character bone mapper

---
+++ api/sunaba/spatial/CharacterBoneMapper.hx
@@ -0,0 +1,20 @@
+﻿package sunaba.spatial;
+import sunaba.core.Reference;
+
+class CharacterBoneMapper extends Behavior {
+	public override function onStart() {
+		var resource = ResourceLoaderService.load("res://Engine/new_bone_map.tres");
+		var boneMap: BoneMap = Reference.castTo(resource, BoneMap);
+
+		var skeleton = getComponent(Skeleton);
+		if (skeleton != null) {
+			for (i in 0...skeleton.getBoneCount()) {
+				var boneName = skeleton.getBoneName(i);
+				var newBoneName = boneMap.findProfileBoneName(boneName);
+				if (newBoneName != "") {
+					skeleton.setBoneName(i, newBoneName);
+				}
+			}
+		}
+	}
+}
