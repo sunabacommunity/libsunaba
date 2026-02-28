d72377d35551b237492ec57300a01711569a6b93
Lucas C. Abbas <lucasbear9@gmail.com>
2026-02-28 01:44:08 -0900

    Added Test21 project

---
+++ demo/tests/test21/src/test21/CharacterLoader.hx
@@ -0,0 +1,25 @@
+﻿package test21;
+import sunaba.Behavior;
+import sunaba.spatial.CharacterData;
+import sunaba.spatial.FaceTextureData;
+import sunaba.Prefab;
+import sunaba.spatial.CharacterRig;
+import sunaba.core.Color;
+
+class CharacterLoader extends Behavior {
+	public override function onStart(){
+		var characterData = new CharacterData();
+		characterData.faceTexture = new FaceTextureData();
+		characterData.faceTexture.load("basechar://data/face_green_xl.ftd");
+		characterData.skinTone = Color.html("#ffdbac");
+
+		var maleCharacterPrefab = new Prefab();
+		maleCharacterPrefab.load("basechar://models/MaleModel.smdl");
+		var maleCharacter = maleCharacterPrefab.instance();
+		maleCharacter.hidden = true;
+		var rig = maleCharacter.getComponent(CharacterRig);
+		entity.addChild(maleCharacter);
+		rig.onStart();
+		rig.loadCharacterData(characterData);
+	}
+}
