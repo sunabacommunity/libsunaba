d6ec9eb67223c34809d3096d633bb6c0e3aabd85
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-03 17:42:50 -0900

    Added Test22 Project

---
+++ demo/tests/test22/src/Main.hx
@@ -0,0 +1,16 @@
+package;
+
+import sunaba.App;
+import sunaba.spatial.Camera;
+import sunaba.spatial.CharacterBoneMapper;
+import sunaba.spatial.CharacterLoader;
+import sunaba.spatial.Skeleton;
+
+class Main extends App {
+	public static function main() {
+		new Main();
+	}
+
+	public override function init() {
+	}
+}
