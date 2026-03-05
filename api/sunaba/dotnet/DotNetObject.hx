4eb16b73fdffeceaedb0a3b7ca56c5ba579e7400
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-04 16:07:06 -0900

    Added DotNetObject.hx

---
+++ api/sunaba/dotnet/DotNetObject.hx
@@ -0,0 +1,35 @@
+﻿package sunaba.dotnet;
+import sunaba.core.Reference;
+import sunaba.core.native.NativeObject;
+import sunaba.core.native.NativeReference;
+import sunaba.core.Variant;
+import sunaba.core.ArrayList;
+
+class DotNetObject extends Reference {
+	public override function nativeInit(?_native: NativeReference) {
+		if (_native == null) {
+			throw "DotNetObject cannot be instantiated directly";
+		}
+		native = _native;
+	}
+
+	public function getMember(name: String): Variant {
+		var args = new ArrayList();
+		args.append(name);
+		return native.call("GetMember", args);
+	}
+
+	public function setMember(name: String, variant: Variant) {
+		var args = new ArrayList();
+		args.append(name);
+		args.append(variant);
+		native.call("SetMember", args);
+	}
+
+	public function callMethod(name: String, args: ArrayList): Variant {
+		var _args = new ArrayList();
+		_args.append(name);
+		_args.append(args);
+		return native.call("CallMethod", _args);
+	}
+}
