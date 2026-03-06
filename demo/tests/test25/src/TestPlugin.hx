e8719dcefc6d015145b2e3deafd7ea71783f8165
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-06 11:20:50 -0900

    Added Test25 project

---
+++ demo/tests/test25/src/TestPlugin.hx
@@ -0,0 +1,10 @@
+import sunaba.studio.Plugin;
+
+class TestPlugin extends Plugin {
+    public static function main() {
+        new TestPlugin();
+    }
+
+    public override function init() {
+    }
+}
