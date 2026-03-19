eb615089ac59909d4464d23528151f93c4644b96
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-19 13:36:07 -0800

    Updated Test25

--- demo/tests/test25/src/Main.hx
+++ demo/tests/test25/src/Main.hx
@@ -1,10 +1,14 @@
 package;
 
 import sunaba.App;
-import sunaba.spatial.Camera;
 import sunaba.spatial.CharacterBoneMapper;
 import sunaba.spatial.CharacterLoader;
-import sunaba.spatial.Skeleton;
+import sunaba.SceneFile;
+import sunaba.spatial.Camera;
+import sunaba.ResourceLoaderService;
+import sunaba.Environment;
+import sunaba.Node;
+import sunaba.core.native.NativeObject;
 
 class Main extends App {
 	public static function main() {
@@ -12,5 +16,17 @@ class Main extends App {
 	}
 
 	public override function init() {
+		var sceneFile = new SceneFile();
+		sceneFile.load("app://TestScene.vscn");
+		
+		var scene = sceneFile.instance();
+		
+		var envRes = ResourceLoaderService.load("res://Engine/Environments/new_environment.tres");
+		var environment = new Environment(envRes.native);
+		var worldEnv = new Node(new NativeObject("WorldEnvironment"));
+		worldEnv.native.set("environment", environment.native);
+		rootNode.addChild(worldEnv);
+
+		rootNode.addChild(scene);
 	}
 }
